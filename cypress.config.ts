b1cdf7de764df66543417c196e956475de31ea49
Andrey K. <karpovich.andrey@gmail.com>
2025-02-02 11:20:10 +0200

    cypress baseUrl

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: '4wfd7y',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    'baseUrl': 'http://localhost:4200'
   },
 
   component: {
